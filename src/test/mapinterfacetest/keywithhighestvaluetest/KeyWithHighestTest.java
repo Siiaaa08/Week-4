0eb2b19ba291fb5c1f7df2cdd497828c4e714d72
Siiaaa08 <salonibaruaa810@gmail.com>
2025-02-14 11:36:53 -0800

    [Saloni]Add Testing of find the key with the highest value

---
+++ src/test/mapinterfacetest/keywithhighestvaluetest/KeyWithHighestTest.java
@@ -0,0 +1,23 @@
+package mapinterfacetest.keywithhighestvaluetest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static mapinterface.keywithhighestvalue.KeyWithHighest.maxKey;
+
+public class KeyWithHighestTest {
+        @Test
+        public void Tester(){
+            Map<String,Integer> input=new HashMap<>();
+            input.put("A",10);
+            input.put("B",40);
+            input.put("C",20);
+            String ans=maxKey(input);
+            Assert.assertEquals("B",ans);
+        }
+    }
+
+
