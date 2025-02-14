fe6cdb199c87a9c9ae99b1763268eb247046a00f
Siiaaa08 <salonibaruaa810@gmail.com>
2025-02-13 20:09:31 -0800

    [Saloni]Add Generate binary numbers using a queue

---
+++ src/main/queueinterface/generatebinarynumbers/BinaryFunction.java
@@ -0,0 +1,19 @@
+package queueinterface.generatebinarynumbers;
+
+import java.util.Queue;
+
+public class BinaryFunction {
+        public static void binaryFunction(int N, Queue<String> q1, Queue<String> q2){
+            if(N==0){
+                return;
+            }
+            String current=q1.poll();
+            q2.add(current);
+            q1.add(current+"0");
+            q1.add(current+"1");
+
+            binaryFunction(N-1,q1,q2);
+        }
+    }
+
+
---
+++ src/main/queueinterface/generatebinarynumbers/Main.java
@@ -0,0 +1,20 @@
+package queueinterface.generatebinarynumbers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static queueinterface.generatebinarynumbers.BinaryFunction.binaryFunction;
+
+public class Main {
+        public static void main(String[] args) {
+            Queue<String> q1=new LinkedList<>();
+            int N=5;
+            q1.add("1");
+            Queue<String> q2=new LinkedList<>();
+            binaryFunction(N,q1,q2);
+            System.out.println(q2);
+
+        }
+    }
+
+
