4d150555c4637629761b04c1bedf19e9ef584aa3
Siiaaa08 <salonibaruaa810@gmail.com>
2025-02-13 20:17:44 -0800

    [Saloni]Add Word Frequency Counter

---
+++ src/main/mapinterface/wordfrequencycounter/Main.java
@@ -0,0 +1,14 @@
+package mapinterface.wordfrequencycounter;
+
+import static mapinterface.wordfrequencycounter.WordFrequency.frequency;
+
+public class Main {
+        public static void main(String[] args) {
+            String input="Hello world, hello Java";
+            frequency(input);
+        }
+
+
+    }
+
+
