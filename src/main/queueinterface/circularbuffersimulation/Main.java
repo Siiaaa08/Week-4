2ca149cdae711c0b3f0998b45244189116bdc72a
Siiaaa08 <salonibaruaa810@gmail.com>
2025-02-13 20:15:35 -0800

    [Saloni]Add Circular Buffer Simulation

---
+++ src/main/queueinterface/circularbuffersimulation/Main.java
@@ -0,0 +1,23 @@
+package queueinterface.circularbuffersimulation;
+
+public class Main {
+        public static void main(String[] args) {
+            Buffer cb = new Buffer(3);
+
+            cb.insert(1);
+            cb.insert(2);
+            cb.insert(3);
+            cb.display();
+
+            cb.insert(4);
+            cb.display();
+
+            cb.insert(5);
+            cb.display();
+
+            System.out.println("Peek oldest: " + cb.peek());
+
+        }
+    }
+
+
