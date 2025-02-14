9b0042957663853ea65347c9479268ae5f462ae9
Siiaaa08 <salonibaruaa810@gmail.com>
2025-02-13 20:22:26 -0800

    [Saloni]Add Merge Two Maps

---
+++ src/main/mapinterface/mergetwomaps/Main.java
@@ -0,0 +1,23 @@
+package mapinterface.mergetwomaps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static mapinterface.mergetwomaps.MergeMap.mergeMap;
+
+public class Main {
+        public static void main(String[] args) {
+            Map<String,Integer> map1=new HashMap<>();
+            map1.put("A",1);
+            map1.put("B",2);
+            map1.put("C",1);
+
+            Map<String,Integer> map2=new HashMap<>();
+            map2.put("A",1);
+            map2.put("D",2);
+            Map<String,Integer> result=mergeMap(map1,map2);
+            System.out.println(result);
+        }
+    }
+
+
