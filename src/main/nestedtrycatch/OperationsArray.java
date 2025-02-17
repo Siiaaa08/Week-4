37ad0d24f122095aa681940f05898f5e4b970cc8
Siiaaa08 <salonibaruaa810@gmail.com>
2025-02-17 10:46:01 -0800

    [Saloni]Add Nested Try-Catch Block

---
+++ src/main/nestedtrycatch/OperationsArray.java
@@ -0,0 +1,16 @@
+package nestedtrycatch;
+
+public class OperationsArray {
+
+        public static int checkArray(int[] arr,int index){
+            int ans=0;
+            try{
+                ans= arr[index];
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Please enter valid index");
+            }
+            return ans;
+        }
+    }
+
+
