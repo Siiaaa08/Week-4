8318ae51d628c4efd5aff77dc7cf51021538c46c
Siiaaa08 <salonibaruaa810@gmail.com>
2025-02-17 10:42:49 -0800

    [Saloni]Add Finally Block execution

---
+++ src/main/finallyblock/IntegerDivision.java
@@ -0,0 +1,18 @@
+package finallyblock;
+
+public class IntegerDivision {
+        public static int division(int a,int b){
+            int ans=0;
+            try{
+                ans=a/b;
+                System.out.println(ans);
+            }catch(ArithmeticException e){
+                System.out.println(e.getMessage());
+            }
+            finally {
+                System.out.println("Operation fully executed!");
+            }
+            return ans;
+        }
+    }
+
---
+++ src/main/finallyblock/Main.java
@@ -0,0 +1,26 @@
+package finallyblock;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static finallyblock.IntegerDivision.division;
+
+public class Main {
+
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            try{
+                System.out.print("Enter the Number :- ");
+                int a=sc.nextInt();
+                System.out.print("Enter the Number :- ");
+                int b=sc.nextInt();
+                division(a,b);
+
+
+            }catch (InputMismatchException e){
+                System.out.println("Please enter integer");
+            }
+        }
+    }
+
+
